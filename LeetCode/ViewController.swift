11609fe4dca55b65f81356e6e13b8e0439de4433
liubang0909 <1548033152@qq.com>
2021-04-09 15:53:05 +0800

    近期刷题同步
    近期刷题同步

--- LeetCode/ViewController.swift
+++ LeetCode/ViewController.swift
@@ -8,16 +8,345 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.view.backgroundColor = .red
-        var array = [0,0,1,1,1,2,2,3,3,4]
-        print(self.removeDuplicates(&array))
+        
+        //        let node1 = ListNode(1)
+        //        let node2 = ListNode(2)
+        //        let node3 = ListNode(3)
+        //        let node4 = ListNode(4)
+        //        node1.next = node2
+        //        node2.next = node3
+        //        node3.next = node4
+        //       print(self.hasCycle(node1))
+        
+        //        print(self.buildTree([3,9,20,15,7], [9,3,15,20,7]) as Any)
+        
+        //        print(self.sortedArrayToBST([-10,-3,0,5,9]) as Any)
+        
+        //二叉树的深度 测试数据
+        //        let root : TreeNode = TreeNode(3);
+        //        let node1 : TreeNode = TreeNode(9);
+        //        let node2 : TreeNode = TreeNode(20);
+        //        let node3 : TreeNode = TreeNode(15);
+        //        let node4 : TreeNode = TreeNode(7);
+        //        root.left = node1;
+        //        root.right = node2;
+        //        node2.left = node3;
+        //        node2.right = node4;
+        //        3,9,20,null,null,15,7
+        //        print(self.maxDepth(root))
+        
+        //合并二叉树  测试数据
+        
+        //                let root1 : TreeNode = TreeNode(1);
+        //                let node1 : TreeNode = TreeNode(3);
+        //                let node2 : TreeNode = TreeNode(2);
+        //                let node3 : TreeNode = TreeNode(5);
+        //                root1.left = node1;
+        //                root1.right = node2;
+        //                node2.left = node3;
+        //
+        //        let root2 : TreeNode = TreeNode(2);
+        //        let node4 : TreeNode = TreeNode(1);
+        //        let node5 : TreeNode = TreeNode(3);
+        //        let node6 : TreeNode = TreeNode(4);
+        //        let node7 : TreeNode = TreeNode(7);
+        //
+        //        root2.left = node4;
+        //        root2.right = node5;
+        //        node4.right = node6;
+        //        node5.right = node7;
+        //        print(self.mergeTrees(root1, root2) as Any)
+        
+        //二叉树的镜像 测试数据
+        //        let root: TreeNode = TreeNode(4)
+        //        let root1: TreeNode = TreeNode(2)
+        //        let root2: TreeNode = TreeNode(7)
+        //        root.left = root1
+        //        root.right = root2
+        //        let root3: TreeNode = TreeNode(1)
+        //        let root4: TreeNode = TreeNode(3)
+        //        root1.left = root3
+        //        root1.right = root4
+        //        let root5: TreeNode = TreeNode(6)
+        //        let root6: TreeNode = TreeNode(7)
+        //        root2.left = root5
+        //        root2.right = root6
+        //        print(self.mirrorTree(root) as Any)
+        
+        //平衡二叉树 测试数据
+        // [3,9,20,null,null,15,7]
+//
+//        let root : TreeNode = TreeNode(3);
+//        let root1: TreeNode = TreeNode(9);
+//        let root2: TreeNode = TreeNode(20);
+//        root.left = root1;
+//        root.right = root2;
+//        let root3: TreeNode = TreeNode(15);
+//        let root4: TreeNode = TreeNode(7);
+//        root2.left = root3;
+//        root2.right = root4;
+//
+        
+        //[1,2,2,3,3,null,null,4,4]
+//        let tempRoot:TreeNode = TreeNode(1);
+//        let tempRoot1:TreeNode = TreeNode(2);
+//        let tempRoot2:TreeNode = TreeNode(2);
+//        tempRoot.left = tempRoot1;
+//        tempRoot.right = tempRoot2;
+//        let tempRoot3:TreeNode = TreeNode(3);
+//        let tempRoot4:TreeNode = TreeNode(3);
+//        tempRoot1.left = tempRoot3;
+//        tempRoot1.right = tempRoot4;
+//        let tempRoot5:TreeNode = TreeNode(4);
+//        let tempRoot6:TreeNode = TreeNode(4);
+//        tempRoot3.left = tempRoot5;
+//        tempRoot3.right = tempRoot6;
+//
+//
+//        print(self.isBalanced(root));
+//        print(self.isBalanced(tempRoot));
+//
+     //   翻转二叉树 测试数据
+        
+//        let root : TreeNode = TreeNode(4);
+//        let root1: TreeNode = TreeNode(2);
+//        let root2: TreeNode = TreeNode(7);
+//        root.left = root1;
+//        root.right = root2;
+//        let root3: TreeNode = TreeNode(1);
+//        let root4: TreeNode = TreeNode(3);
+//        root2.left = root3;
+//        root2.right = root4;
+//        let root5: TreeNode = TreeNode(6);
+//        let root6: TreeNode = TreeNode(9);
+//        root3.left = root5;
+//        root3.right = root6;
+//        print(self.invertTree(root)as Any);
+        
+        
+        
+    }
+    
+    
+    public class TreeNode {
+        public var val: Int
+        public var left: TreeNode?
+        public var right: TreeNode?
+        public init(_ val: Int) {
+            self.val = val
+            self.left = nil
+            self.right = nil
+        }
+    }
+    
+    // MARK: -  翻转二叉树
+    
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+
+        if(root == nil)
+        {
+            return nil
+        }
+        let endRoot : TreeNode = TreeNode(root?.val ?? 0);
+        if root?.left != nil{
+            
+            endRoot.right =  self.invertTree(root?.left)
+        }
+        if root?.right != nil{
+            
+            endRoot.left =  self.invertTree(root?.right)
+        }
+        
+        return endRoot
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    // MARK: -  平衡二叉树
+    func isBalanced(_ root: TreeNode?) -> Bool {
+        if root == nil
+        {
+            return true
+        }
+        
+        let leftDepth = self.treeDepth(root?.left);
+        let rightDepth = self.treeDepth(root?.right);
+
+        return abs(leftDepth - rightDepth) <= 1 && self.isBalanced(root?.left) && isBalanced(root?.right);
 
     }
     
+    func treeDepth(_ root: TreeNode?) -> Int {
+        
+        if root == nil {
+            return 0
+        }
+        return  max(self.treeDepth(root?.left), self.treeDepth(root?.right))+1
+        
+    }
+    
+    
+    
+    
+    
+    // MARK: -  二叉树的镜像
+    func mirrorTree(_ root: TreeNode?) -> TreeNode? {
+        
+        if(root == nil)
+        {
+            return nil
+        }
+        
+        let endRoot : TreeNode = TreeNode(root?.val ?? 0);
+        
+        if root?.left != nil {
+            endRoot.right = self.mirrorTree(root?.left)
+        }
+        if (root?.right != nil)
+        {
+            endRoot.left = self.mirrorTree(root?.right)
+        }
+        
+        return   endRoot
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    // MARK: - 合并二叉树
+    func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
+        
+        if root1 == nil{
+            
+            return root2
+        }
+        if root2 == nil {
+            
+            return root1
+        }
+        
+        
+        let root: TreeNode = TreeNode((root1?.val ?? 0 ) + (root2?.val ?? 0 ) )
+        
+        root.left = self.mergeTrees(root1?.left, root2?.left)
+        root.right = self.mergeTrees(root1?.right, root2?.right)
+        
+        return root
+        
+    }
+    
+    
+    
+    
+    
+    
+    // MARK: - 二叉树的深度
+    
+    func maxDepth(_ root: TreeNode?) -> Int {
+        if root == nil {
+            return 0
+        }
+        return  max(self.maxDepth(root?.left),self.maxDepth(root?.right))+1
+    }
+    
+    
+    
+    
+    
+    // MARK: - 最小高度树
+    func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
+        
+        let arrayLength = nums.count
+        if arrayLength <= 0
+        {
+            return nil
+        }
+        
+        let m = arrayLength/2
+        let root:TreeNode  = TreeNode(nums[m])
+        root.left = self.sortedArrayToBST(Array(nums.prefix(m)))
+        if(arrayLength%2 == 0)
+        {
+            root.right = self.sortedArrayToBST(Array(nums.suffix(m - 1)))
+        }
+        else
+        {
+            root.right = self.sortedArrayToBST(Array(nums.suffix(m)))
+        }
+        return root
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    //    func buildTree(_ preorder: [Int], _ inorder: [Int]) -> TreeNode? {
+    //
+    //        let rootNode = TreeNode(preorder[0])
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //        return rootNode
+    //    }
+    
+    
+    
+    
+    
+    
+    // MARK: - 判断链表是否有环
+    
+    func hasCycle(_ head: ListNode?) -> Bool {
+        
+        //空的或者只有一个的没有环
+        if head == nil || head?.next == nil {
+            return false
+        }
+        
+        
+        var slow = head
+        var fast = head?.next
+        
+        while fast != nil, fast?.next != nil {
+            
+            slow = slow?.next
+            fast = fast?.next?.next
+            if slow === fast {
+                return true
+            }
+            
+        }
+        
+        return false
+    }
     
     
     
@@ -29,17 +358,17 @@ class ViewController: UIViewController {
     
     // MARK: - 删除排序数组中的重复项
     func removeDuplicates(_ nums: inout [Int]) -> Int {
-         
+        
         if (nums.count == 0) { return 0 }
-                    var i = 0
-                    for j in 1..<nums.count {
-                        if (nums[i] != nums[j]) {
-                            i = i + 1
-                            nums[i] = nums[j]
-                        }
-                    }
-                    return (i + 1)
-
+        var i = 0
+        for j in 1..<nums.count {
+            if (nums[i] != nums[j]) {
+                i = i + 1
+                nums[i] = nums[j]
+            }
+        }
+        return (i + 1)
+        
     }
     
     
@@ -51,12 +380,12 @@ class ViewController: UIViewController {
     // MARK: - 有效的括号
     
     func isValid(_ s: String) -> Bool {
-
+        
         if(s.count%2 != 0 || s.count == 0 )
         {
             return false
         }
-    
+        
         var  array = [Character]()
         
         for char in s {
@@ -78,7 +407,7 @@ class ViewController: UIViewController {
                 print(array)
                 
                 if  array.contains(char) && array.last == char {
-                
+                    
                     array.remove(at: array.count-1)
                 }
                 else // 说明栈里没有可以匹配的
@@ -110,7 +439,7 @@ class ViewController: UIViewController {
     
     
     func longestCommonPrefix(_ strs: [String]) -> String {
-
+        
         
         if(strs.count==0)
         {
@@ -124,7 +453,7 @@ class ViewController: UIViewController {
         
         var pubStr = ""
         var tempStr = ""
-
+        
         var flag = false
         
         let firstStr = strs[0]
@@ -133,17 +462,17 @@ class ViewController: UIViewController {
             
             if flag
             {
-               break
+                break
             }
-
+            
             let tempIndex = firstStr.index(firstStr.startIndex, offsetBy: index)
             tempStr = String(firstStr[firstStr.startIndex..<tempIndex])
             
             for  (strIndex ,str) in strs.enumerated() {
-
+                
                 if(!str.hasPrefix(tempStr))
                 {
-                   flag = true
+                    flag = true
                     break
                 }
                 if(strIndex == strs.count - 1)
@@ -151,11 +480,11 @@ class ViewController: UIViewController {
                     
                     pubStr = tempStr
                 }
-
+                
             }
-                        
+            
         }
-       
+        
         return pubStr
     }
     
@@ -163,14 +492,14 @@ class ViewController: UIViewController {
     
     // MARK: - 整数转罗马数字
     func intToRoman(_ num: Int) -> String {
-
+        
         
         var tempNum = num
         
         var endStr = ""
         let keys = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
         let valueArray = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
-
+        
         
         for  (index,key) in keys.enumerated() {
             if(tempNum==0)
@@ -219,28 +548,28 @@ class ViewController: UIViewController {
             else
             {
                 nums1.append(value)
-
+                
             }
         }
         nums1 = nums1.sorted(by:<)
-       
-
+        
+        
     }
     
     
     
     
     public class ListNode {
-         public var val: Int
-         public var next: ListNode?
-         public init(_ val: Int) {
-             self.val = val
-             self.next = nil
-         }
-     }
-
+        public var val: Int
+        public var next: ListNode?
+        public init(_ val: Int) {
+            self.val = val
+            self.next = nil
+        }
+    }
+    
     func reverseList(_ head: ListNode?) -> ListNode? {
-     
+        
         var oldHead = head
         var newHead:ListNode? = nil
         
@@ -252,7 +581,7 @@ class ViewController: UIViewController {
             oldHead = temp
         }
         return newHead
-
+        
     }
     
     
@@ -277,16 +606,16 @@ class ViewController: UIViewController {
         
         for index in 0...maxIndex {
             
-           let temp = s[s.count-1-index]
+            let temp = s[s.count-1-index]
             s[s.count-1-index] = s[index]
             s[index] = temp
             
         }
         
         print(s)
-
-    
-    
+        
+        
+        
     }
     
     
@@ -307,12 +636,12 @@ class ViewController: UIViewController {
             
             if(tempStr.contains(key))
             {
-
-              tempStr.removeSubrange(tempStr.range(of:key)!)
-              sum += value                
+                
+                tempStr.removeSubrange(tempStr.range(of:key)!)
+                sum += value
             }
         }
-       
+        
         for char in tempStr {
             
             sum += valueDict[String(char)] ?? 0
@@ -320,16 +649,16 @@ class ViewController: UIViewController {
         
         return sum
         
-    
+        
     }
     
     
     
     // MARK: - 回文数
-
-    func isPalindrome(_ x: Int) -> Bool {
-          
     
+    func isPalindrome(_ x: Int) -> Bool {
+        
+        
         if(x<0)//负数肯定不是回文数
         {
             return false
@@ -345,15 +674,15 @@ class ViewController: UIViewController {
         {
             maxIndex = array.count/2-1
         }
-       
+        
         for index in 0...maxIndex{
-         if array[index] != array[array.count-1-index]{
-          return false
+            if array[index] != array[array.count-1-index]{
+                return false
+            }
+            
         }
-                
-    }
         return true
-
+        
         
         
     }
@@ -371,35 +700,35 @@ class ViewController: UIViewController {
         //去除空格
         let whitespace = NSCharacterSet.whitespacesAndNewlines
         let noWhitespaceStr = s.trimmingCharacters(in: whitespace)
-
+        
         var sign = 1
-                
+        
         var  result = 0
-          
+        
         
         for (index, scalars) in noWhitespaceStr.unicodeScalars.enumerated() {
-                    if ((scalars.value >= 48 && scalars.value <= 57) || (scalars.value == 45 && index == 0)) {
-                        //如果第一位是字符 - 则标识结果为负数
-                        if scalars.value == 45 && index == 0 {
-                            sign = -1
-                            continue
-                        }
-                        
-                        //判断依据：当前结果大于最大值除10 (214748364)
-                        if result > Int32.max / 10 || (result == Int32.max / 10 && Int(String(scalars))! > 7) {
-                            return sign == 1 ? Int(Int32.max) : Int(Int32.min)
-                        }
-                        
-                        result = result * 10 + Int(String(scalars))!
-                    }else if scalars.value == 43 && index == 0 {
-                        continue
-                    }else {
-                        break
-                    }
-         
+            if ((scalars.value >= 48 && scalars.value <= 57) || (scalars.value == 45 && index == 0)) {
+                //如果第一位是字符 - 则标识结果为负数
+                if scalars.value == 45 && index == 0 {
+                    sign = -1
+                    continue
                 }
-                return result * sign
-
+                
+                //判断依据：当前结果大于最大值除10 (214748364)
+                if result > Int32.max / 10 || (result == Int32.max / 10 && Int(String(scalars))! > 7) {
+                    return sign == 1 ? Int(Int32.max) : Int(Int32.min)
+                }
+                
+                result = result * 10 + Int(String(scalars))!
+            }else if scalars.value == 43 && index == 0 {
+                continue
+            }else {
+                break
+            }
+            
+        }
+        return result * sign
+        
     }
     
     
@@ -412,7 +741,7 @@ class ViewController: UIViewController {
     // MARK: - 无重复字符的最长子串
     
     func lengthOfLongestSubstring(_ s: String) -> Int {
-   
+        
         var m = 0
         
         var lists:[Character] = []
@@ -425,12 +754,11 @@ class ViewController: UIViewController {
                 
                 lists.removeSubrange(0...index)
             }
-    
+            
             lists.append(sub)
         }
         
         return max(m, lists.count)
-                
         
         
         
@@ -441,7 +769,8 @@ class ViewController: UIViewController {
         
         
         
-
+        
+        
     }
     
     
@@ -452,9 +781,9 @@ class ViewController: UIViewController {
     
     
     // MARK: - 整数反转
-
+    
     func reverse(_ x: Int) -> Int {
-                
+        
         var numberArray = [Int]()
         var temp = x
         var plus = 1
@@ -464,7 +793,7 @@ class ViewController: UIViewController {
             plus = -1
             
         }
-
+        
         while true {
             if(temp<10)
             {
@@ -473,15 +802,15 @@ class ViewController: UIViewController {
             }
             numberArray.append(temp%10)
             temp = temp/10;
-
+            
             
         }
         print(numberArray)
         var endValue = 0;
         let  spaceValue = Int(pow(2.0,31.0))
-
+        
         for index in 0..<numberArray.count {
-                        
+            
             endValue +=  numberArray[index] * Int(pow(10.0,Double(numberArray.count-1-index)))
             if(endValue>=spaceValue)
             {
@@ -493,19 +822,19 @@ class ViewController: UIViewController {
         endValue = endValue * plus;
         
         
-
+        
         if (endValue >= -spaceValue) && (endValue <= spaceValue-1)
         {
             return endValue
-
+            
         }
         
         return 0
-
         
         
         
-
+        
+        
     }
     
     
@@ -519,7 +848,7 @@ class ViewController: UIViewController {
     
     
     
-
+    
     // MARK: - 两数之和
     func twoSum(_ nums:[Int], _ target:Int ) -> [Int] {
         
@@ -527,21 +856,21 @@ class ViewController: UIViewController {
             for nextIndex in index+1..<nums.count {
                 
                 if nums[index]+nums[nextIndex]==target {
-
+                    
                     return [index,nextIndex];
                 }
             }
-        
+            
         }
         
         return [0,0];
-
+        
         
     }
     
     
     
-
-
+    
+    
 }
 
